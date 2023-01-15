8015a779acd686a99961b0ae167c92cc05293771
Thomas Delaney <thomas.delaney@posteo.net>
2023-01-15 01:16:46 +0100

    Updated Items.py

--- Item.py
+++ Item.py
@@ -1,28 +1,36 @@
 """A class for defining ToDoList Items"""
-import uuid
+import json
 
 
 class Item:
     """Attributes:
-        id:
         name:
         description:
     Functions:
-        get_methods: for id, name, description"""
+        get_methods: for name, description"""
     def __init__(self, name: str, description: str) -> None:
         """For initialising the object.
         Args:
             name: the name of the list item
             description: a short description of the item"""
-        self.id: str = uuid.uuid4().hex
         self.name: str = name
         self.description: str = description
-
-    def get_id(self) -> str:
-        return self.id
 
     def get_name(self) -> str:
         return self.name
 
     def get_description(self) -> str:
         return self.description
+
+    @staticmethod
+    def load_by_name(name: str) -> 'Item':
+        item = Item('', '')
+        with open('tasks.json', 'r') as f:
+            tasks_dict = json.load(f)
+        for task_name, desc in tasks_dict.items():
+            if name == task_name:
+                item = Item(name=task_name, description=desc)
+        return item
+
+    def __repr__(self) -> str:
+        return f'<Item: {self.name}: {self.description}>'
